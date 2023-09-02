92c351bd9d69f9eef7f2e231a57fb9d672b56608
João_Lucas Silva <joaolucasacademico@gmail.com>
2023-09-02 04:11:48 -0300

    refactor: Moved feature verification to a module

--- Product01/sevensegmentdisplay.v
+++ Product01/sevensegmentdisplay.v
@@ -1,54 +1,34 @@
 module sevensegmentdisplay (A, B, C,
-D, E, F,
-G, H, I,
+dpval,
 sega, segb, segc, segd, sege,segf, segg);
 
-	input A, B, C,
-	D, E, F,
-	G, H, I;
+	input A, B, C, dpval;
 
 	output sega, segb, segc, segd, sege, segf, segg;
 
 	wire NA, NB, NC,
-	ND, NF,
-	NG, NI,
 	and0wire, and1wire, and2wire, and3wire, and4wire,
-	segbor, segcor, segeor,segfor, seggor,
-	ft2chkand1wire, ft2chkand2wire, ft2chkwire;
+	segbor, segcor, segeor,segfor, seggor;
 
 not (NA, A);
 not (NB, B);
 not (NC, C);
-
-not (ND, D);
-not (NF, F);
-
-not (NG, G);
-not (NI, I);
 
 and and0 (and0wire, NA, C);
 and and1 (and1wire, A, B, NC);
 and and2 (and2wire, NB, C);
 and and3 (and3wire, NA, B, C);
 
-//or (segaor);
 or (segbor, and0wire, and1wire, and2wire);
 or (segcor, and2wire, and1wire);
-//or (segdor);
-//or (segeor);
-//or (segfor);
 or (seggor, and0wire, and1wire);
 
-and ft2chkand1 (ft2chkand1wire, ND, E, NF);
-and ft2chkand2 (ft2chkand2wire, NG, H, NI);
-or ft2chkor (ft2chkwire, ft2chkand1wire, ft2chkand2wire);
-
-nand (sega, and0wire, ft2chkwire);
-nand (segb, segbor, ft2chkwire);
-nand (segc, segcor, ft2chkwire);
-nand (segd, and0wire, ft2chkwire);
-nand (sege, and3wire, ft2chkwire);
-nand (segf, and1wire, ft2chkwire);
-nand (segg, seggor, ft2chkwire);
+nand (sega, and0wire, dpval);
+nand (segb, segbor, dpval);
+nand (segc, segcor, dpval);
+nand (segd, and0wire, dpval);
+nand (sege, and3wire, dpval);
+nand (segf, and1wire, dpval);
+nand (segg, seggor, dpval);
 
 endmodule
