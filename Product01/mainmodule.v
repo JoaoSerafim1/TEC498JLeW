cddc3ccf9ecaa2862d2ab7609bbff3343e83be3d
João_Lucas Silva <joaolucasacademico@gmail.com>
2023-09-02 04:15:28 -0300

    feat: Added instances of new obejcts

--- Product01/mainmodule.v
+++ Product01/mainmodule.v
@@ -14,11 +14,31 @@ SGDA, SGDB, SGDC, SGDD, SGDE, SGDF, SGDG); //Declara todos os elementos de entra
 	
 	//Declara os fios utilizados para conectar os modulos
 	wire NBT0, NBT1, NBT2, NBT3,
+	
 	permIE01, permIE02,
-	priorselwire, cftchkwire, sevenselTo6to3muxdisplay,
-	Npriorselwire,
-	mux6to3ToDisplay0, mux6to3ToDisplay1, mux6to3ToDisplay2;
-
+	
+	priorselwire, Npriorselwire, cftchkwire,
+	
+	featurebitIE01[2:0],
+	featurebitIE02[2:0],
+	
+	sevenselwire,
+	mux6to3ToValidator0, mux6to3ToValidator1, mux6to3ToValidator2,
+	displayvalwire,
+	
+	mux6to3ToDisplay0, mux6to3ToDisplay1, mux6to3ToDisplay2,
+	
+	ledselwire0,
+	ledselwire1,
+	
+	ledwireIE01 [6:0],
+	ledwireIE02 [6:0],
+	
+	lattledwireIE01[6:0],
+	lattledwireIE02[6:0],
+	
+	seqledwireIE01[6:0],
+	seqledwireIE02[6:0];
 
 //Negacao dos botoes
 not (NBT0, BT0);
@@ -29,7 +49,7 @@ not (NBT3, BT3);
 //Negacao do seletor de prioridade do BUS Transceiver da IE01
 not (Npriorselwire, priorselwire);
 
-allpermission allperm_0 (
+allpermission allperm_IE01 (
 		.A (CH7),
 		.B (NBT3),
 		.C (NBT2),
@@ -39,7 +59,7 @@ allpermission allperm_0 (
 		.permout (permIE01)
 );
 
-allpermission allperm_1 (
+allpermission allperm_IE02 (
 		.A (CH3),
 		.B (NBT1),
 		.C (NBT0),
@@ -69,53 +89,60 @@ featureconflictchecker ftcftchk_0 (
 		.cftout (cftchkwire)
 );
 
-unidirbustrans3b busIE01 (
+unidirbustrans3b gatedbus_IE01 (
 		.A0 (CH6),
 		.A1 (CH5),
 		.A2 (CH4),
 		.flowvalve (permIE01),
 		.conflictstatus (cftchkwire),
 		.prioritystatus (Npriorselwire),
-		.B0 (int01foutA),
-		.B1 (int01foutB),
-		.B2 (int01foutC)
+		.B0 (featurebitIE01[0]),
+		.B1 (featurebitIE01[1]),
+		.B2 (featurebitIE01[2])
 );
 
-unidirbustrans3b busIE02 (
+unidirbustrans3b gatedbus_IE02 (
 		.A0 (CH2),
 		.A1 (CH1),
 		.A2 (CH0),
 		.flowvalve (permIE02),
 		.conflictstatus (cftchkwire),
 		.prioritystatus (priorselwire),
-		.B0 (int02foutA),
-		.B1 (int02foutB),
-		.B2 (int02foutC)
+		.B0 (featurebitIE02[0]),
+		.B1 (featurebitIE02[1]),
+		.B2 (featurebitIE02[2])
 );
 
 sevendisplayselector sevensel_0 (
-		.A (int01foutA),
-		.B (int01foutB),
-		.C (int01foutC),
-		.D (int02foutA),
-		.E (int02foutB),
-		.F (int02foutC),
+		.A (featurebitIE01[0]),
+		.B (featurebitIE01[1]),
+		.C (featurebitIE01[2]),
+		.D (featurebitIE02[0]),
+		.E (featurebitIE02[1]),
+		.F (featurebitIE02[2]),
 		.priorsel (priorselwire),
-		.displaysel (sevenselTo6to3muxdisplay)
+		.displaysel (sevenselwire)
 );
 
-/*mux6to3 authmux_0 (
-		.A (CH7),
-		.B (NBT3),
-		.C (NBT2),
-		.D (CH3),
-		.E (NBT1),
-		.F (NBT0),
-		.sel (sevenselTo6to3muxdisplay),
-		.out0 (mux6to3ToDisplay0),
-		.out1 (mux6to3ToDisplay1),
-		.out2 (mux6to3ToDisplay2)
-);*/
+mux6to3 dpvalidatormux_0 (
+		.A (CH2),
+		.B (CH1),
+		.C (CH0),
+		.D (CH6),
+		.E (CH5),
+		.F (CH4),
+		.sel (sevenselwire),
+		.out0 (mux6to3ToValidator0),
+		.out1 (mux6to3ToValidator1),
+		.out2 (mux6to3ToValidator2)
+);
+
+displayvalidator dpval_0 (
+		.A (mux6to3ToValidator0),
+		.B (mux6to3ToValidator1),
+		.C (mux6to3ToValidator2),
+		.validout (displayvalwire)
+);
 
 mux6to3 displaymux_0 (
 		.A (CH7),
@@ -124,7 +151,7 @@ mux6to3 displaymux_0 (
 		.D (CH3),
 		.E (NBT1),
 		.F (NBT0),
-		.sel (sevenselTo6to3muxdisplay),
+		.sel (sevenselwire),
 		.out0 (mux6to3ToDisplay0),
 		.out1 (mux6to3ToDisplay1),
 		.out2 (mux6to3ToDisplay2)
@@ -134,12 +161,7 @@ sevensegmentdisplay ssdisplay_0 (
 		.A (mux6to3ToDisplay0),
 		.B (mux6to3ToDisplay1),
 		.C (mux6to3ToDisplay2),
-		.D (CH2),
-		.E (CH1),
-		.F (CH0),
-		.G (CH6),
-		.H (CH5),
-		.I (CH4),
+		.dpval (displayvalwire),
 		.sega (SGDA),
 		.segb (SGDB),
 		.segc (SGDC),
@@ -148,5 +170,103 @@ sevensegmentdisplay ssdisplay_0 (
 		.segf (SGDF),
 		.segg (SGDG)
 );
+
+ledinterfaceselector ledifsel_IE01 (
+		.A (CH7),
+		.B (NBT3),
+		.C (NBT2),
+		.ledsel (ledselwire0)
+);
+
+ledinterfaceselector ledifsel_IE02 (
+		.A (CH3),
+		.B (NBT1),
+		.C (NBT0),
+		.ledsel (ledselwire1)
+);
+
+ledsequencedecoder ledseqdec_IE01(
+		.A (featurebitIE01[0]),
+		.B (featurebitIE01[1]),
+		.C (featurebitIE01[2]),
+		.out0 (ledwireIE01[0]),
+		.out1 (ledwireIE01[1]),
+		.out2 (ledwireIE01[2]),
+		.out3 (ledwireIE01[3]),
+		.out4 (ledwireIE01[4]),
+		.out5 (ledwireIE01[5]),
+		.out6 (ledwireIE01[6])
+);
+
+ledsequencedecoder ledseqdec_IE02(
+		.A (featurebitIE02[0]),
+		.B (featurebitIE02[1]),
+		.C (featurebitIE02[2]),
+		.out0 (ledwireIE02[0]),
+		.out1 (ledwireIE02[1]),
+		.out2 (ledwireIE02[2]),
+		.out3 (ledwireIE02[3]),
+		.out4 (ledwireIE02[4]),
+		.out5 (ledwireIE02[5]),
+		.out6 (ledwireIE02[6])
+);
+
+mux7to14 mux7to14_IE01(
+		.A (ledwireIE01[0]),
+		.B (ledwireIE01[1]),
+		.C (ledwireIE01[2]),
+		.D (ledwireIE01[3]),
+		.E (ledwireIE01[4]),
+		.F (ledwireIE01[5]),
+		.G (ledwireIE01[6]),
+		.sel (ledselwire0),
+		.out00 (lattledwireIE01[0]),
+		.out01 (lattledwireIE01[1]),
+		.out02 (lattledwireIE01[2]),
+		.out03 (lattledwireIE01[3]),
+		.out04 (lattledwireIE01[4]),
+		.out05 (lattledwireIE01[5]),
+		.out06 (lattledwireIE01[6]),
+		.out07 (seqledwireIE01[0]),
+		.out08 (seqledwireIE01[1]),
+		.out09 (seqledwireIE01[2]),
+		.out10 (seqledwireIE01[3]),
+		.out11 (seqledwireIE01[4]),
+		.out12 (seqledwireIE01[5]),
+		.out13 (seqledwireIE01[6])
+);
+
+mux7to14 mux7to14_IE02(
+		.A (ledwireIE02[0]),
+		.B (ledwireIE02[1]),
+		.C (ledwireIE02[2]),
+		.D (ledwireIE02[3]),
+		.E (ledwireIE02[4]),
+		.F (ledwireIE02[5]),
+		.G (ledwireIE02[6]),
+		.sel (ledselwire1),
+		.out00 (lattledwireIE02[0]),
+		.out01 (lattledwireIE02[1]),
+		.out02 (lattledwireIE02[2]),
+		.out03 (lattledwireIE02[3]),
+		.out04 (lattledwireIE02[4]),
+		.out05 (lattledwireIE02[5]),
+		.out06 (lattledwireIE02[6]),
+		.out07 (seqledwireIE02[0]),
+		.out08 (seqledwireIE02[1]),
+		.out09 (seqledwireIE02[2]),
+		.out10 (seqledwireIE02[3]),
+		.out11 (seqledwireIE02[4]),
+		.out12 (seqledwireIE02[5]),
+		.out13 (seqledwireIE02[6])
+);
+
+or (LED0, lattledwireIE01[0], lattledwireIE02[0]);
+or (LED1, lattledwireIE01[1], lattledwireIE02[1]);
+or (LED2, lattledwireIE01[2], lattledwireIE02[2]);
+or (LED3, lattledwireIE01[3], lattledwireIE02[3]);
+or (LED4, lattledwireIE01[4], lattledwireIE02[4]);
+or (LED5, lattledwireIE01[5], lattledwireIE02[5]);
+or (LED6, lattledwireIE01[6], lattledwireIE02[6]);
 
 endmodule
